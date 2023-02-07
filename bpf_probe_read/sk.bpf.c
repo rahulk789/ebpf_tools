073d78dae604d168570364d9d47ac071676d1b97
Rahul k <83643646+rahulk789@users.noreply.github.com>
2023-02-07 07:40:46 +0000

    fix

--- bpf_probe_read/sk.bpf.c
+++ bpf_probe_read/sk.bpf.c
@@ -23,20 +23,25 @@ const struct pidstruct *unused __attribute__((unused));
 
 SEC("kprobe/sys_execve")
 int pid_matcher(struct pt_regs *ctx) {
-    u64 p,*boolval,present=2,empty=1;
-    u32 index0 = 0;
-    u32 index1 = 1;
-
+    u64 p,*boolval,*matchval,present=2,empty=1;
+    u32 index0 = 0; // current pid
+    u32 index1 = 1; // val is 2 if matched 1 if not matched
+    u32 index2 = 2; //has user asked pid
+    
     p=bpf_get_current_pid_tgid();
     p = p >> 32 ;
-    
     bpf_map_update_elem(&pidcheck,&index0,&p,BPF_ANY);
-    if (p==50000){
-        boolval = bpf_map_lookup_elem(&pidcheck,&index1);
-        if (!boolval){
-            bpf_map_update_elem(&pidcheck, &index1, &present, BPF_ANY);
-            return 0;}
-    }
-        bpf_map_update_elem(&pidcheck,&index1,&empty,BPF_ANY);
+    
+    
+    matchval = bpf_map_lookup_elem(&pidcheck,&index2);
+    if (matchval){
+        if (p==*matchval){
+            boolval = bpf_map_lookup_elem(&pidcheck,&index1);
+            if (boolval){
+                bpf_map_update_elem(&pidcheck, &index1, &present, BPF_ANY);
+                return 0;}
+            }
+        }
+    bpf_map_update_elem(&pidcheck,&index1,&empty,BPF_ANY);
     return 0;
 }

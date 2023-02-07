c23be40dc2412c250a7c59fab0d6073ac528ef06
Rahul k <83643646+rahulk789@users.noreply.github.com>
2023-02-07 07:39:54 +0000

    fix main.go

--- bpf_probe_read/main.go
+++ bpf_probe_read/main.go
@@ -7,7 +7,8 @@ package main
 import (
 	"log"
 	"time"
-
+    "fmt"
+    "flag"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/rlimit"
 )
@@ -16,6 +17,12 @@ import (
 
 
 func main() {
+
+    matchpid := flag.Int64("matchpid",1000,"pid to be matched")
+    flag.Parse()
+    fmt.Println("argument that you have given:", *matchpid)
+    
+
     if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal(err)
 	}
@@ -39,19 +46,22 @@ func main() {
     // The plan here is to store the current pid at 0 , and store 1 at index 1 if current pid matches pid given by user
     // The pid given by the user is stored at index 3 to be accessed by the user.
 	
-    const mapkey1 uint32 = 0
-	const mapkey2 uint32 = 1
+    const mapkey0 uint32 = 0
+	const mapkey1 uint32 = 1
+    const mapkey2 uint32 = 2
+	if err := objs.Pidcheck.Put(mapkey2, matchpid); err != nil {
+		log.Fatalf("reading map: %v", err)
+	}
     for range ticker.C {
         var value1 uint64
         var value2 uint64
         // var value bpfPidstruct
-		if err := objs.Pidcheck.Lookup(mapkey1, &value1); err != nil {
+		if err := objs.Pidcheck.Lookup(mapkey0, &value1); err != nil {
 			log.Fatalf("reading map: %v", err)
 		}
-        if err := objs.Pidcheck.Lookup(mapkey2, &value2); err != nil {
+        if err := objs.Pidcheck.Lookup(mapkey1, &value2); err != nil {
 			log.Fatalf("reading map: %v", err)
 		}
         log.Printf("Current pid %d \t is pid matched?: %d \n",value1,value2)
     }
 }
-

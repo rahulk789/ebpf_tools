8a4f1383c7b8c9650b9cfdad1877f6042b45b46e
rahulk789 <rahul.u.india@gmail.com>
2023-03-07 22:24:07 +0000

    fix: reading comm from perf array

--- pass_4040/tcp.bpf.c
+++ pass_4040/tcp.bpf.c
@@ -56,31 +56,33 @@ int bind_intercept(struct pt_regs *ctx,  const struct sockaddr *addr) {
     bpf_perf_event_output(ctx,&pipe, BPF_F_CURRENT_CPU, &infostruct, sizeof(infostruct));
     return 0;
 }
-/*
-SEC("socket")
+
+SEC("security_socket_bind")
 int socket(struct __sk_buff *skb) {
-  void *data = (void *)(long)skb->data;
-  void *data_end = (void *)(long)skb->data_end;
-  struct ethhdr *eth = data;
-
-  if ((void *)eth + sizeof(*eth) <= data_end) {
-
-    struct iphdr *ip = data + sizeof(*eth);
-    if ((void *)ip + sizeof(*ip) <= data_end) {
-
-      if (ip->protocol == IPPROTO_TCP) {
-
-        struct tcphdr *tcp = (void *)ip + sizeof(*ip);
-        if ((void *)tcp + sizeof(*tcp) <= data_end) {
-            u64 value = (tcp->dest);
-            long bpf_perf_event_read_value(struct bpf_map *map, u64 flags,struct bpf_perf_event_value *buf, u32 buf_size)
-          if (value == 4040 && x == "myprocess" )
-            return SK_PASS;
-          else if (value != 4040)//  && x != "myprocess" )
-            return SK_DROP;
+    struct info *infostruct;
+    u32 key = bpf_get_smp_processor_id();
+    infostruct = bpf_map_lookup_elem(&pipe,&key);
+    if (!infostruct) return 0;
+    u8 Comm[32] = "s";
+    void *data = (void *)(long)skb->data;
+    void *data_end = (void *)(long)skb->data_end;
+    struct ethhdr *eth = data;
+    if (Comm==infostruct->comm){
+        if ((void *)eth + sizeof(*eth) <= data_end) {
+            struct iphdr *ip = data + sizeof(*eth);
+            if ((void *)ip + sizeof(*ip) <= data_end) {
+                if (ip->protocol == IPPROTO_TCP) {
+                    struct tcphdr *tcp = (void *)ip + sizeof(*ip);
+                    if ((void *)tcp + sizeof(*tcp) <= data_end) {
+                        u64 value = (tcp->dest);
+                        if (value == 4040)
+                        return SK_PASS;
+                        else if (value != 4040)
+                        return SK_DROP;
+                    }
+                }
+            }
         }
-      }
     }
-  }
-  return SK_DROP;
-}*/
+    return SK_PASS;
+}

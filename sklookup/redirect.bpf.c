c3146598f57d0b0f93d4c3386b9f4fe10cd9ddf5
rahulk789 <rahul.u.india@gmail.com>
2023-05-21 19:56:14 +0530

    sklookup egress filter

--- sklookup/redirect.bpf.c
+++ sklookup/redirect.bpf.c
@@ -1,38 +1,34 @@
-// +build ignore
 
 #include "vmlinux.h"
+#include <bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
-
+#include <bpf_endian.h>
 #define AF_INET		2
 char __license[] SEC("license") = "Dual MIT/GPL";
+static const __u16 DST_PORT = 4040; 
 
-struct {
-    __uint(type, BPF_MAP_TYPE_SOCKMAP);
-    __uint(max_entries, 32);
-    __type(key, __u32);
-    __type(value, __u64);
-} redir_map SEC(".maps");
+// uncomment the following lines for debugging, output received in " sudo cat  /sys/kernel/debug/tracing/trace_pipe "
 
-static const __u16 DST_PORT = 4000; /* Host byte order */
-static const __u32 KEY_SERVER_A = 0;
-
-/* Redirect packets destined for DST_IP4 address to socket at redir_map[0]. */
 SEC("sk_lookup")
 int redir_ip4(struct bpf_sk_lookup *ctx)
 {
-    struct bpf_sock *sk;
+    struct task_struct *s;
+    char comm[16];
     int err;
-
-    if (ctx->family != AF_INET)
-        return SK_PASS;
-    if (ctx->local_port != DST_PORT)
-        return SK_PASS;
-
-    sk = bpf_map_lookup_elem(&redir_map, &KEY_SERVER_A);
-    if (!sk)
-        return SK_PASS;
-
-    err = bpf_sk_assign(ctx, sk, 0);
-    bpf_sk_release(sk);
-    return err ? SK_DROP : SK_PASS;
+    if (ctx->family == AF_INET) {
+            s = (struct task_struct*)bpf_get_current_task();
+            err = bpf_core_read(&comm, sizeof(comm), &s->comm);
+            if (!err) {
+            bool commcheck = comm[0]=='m' && comm[1]=='y' && comm[2]=='p' && comm[3]=='r' && comm[4]=='o' && comm[5]=='c' && comm[6]=='e' && comm[7]=='s' && comm[8]=='s' && comm[9]=='\0';
+            const char fmt_str[] = "Hello, world, from BPF! My COMM is %s\n";
+            bpf_trace_printk(fmt_str, sizeof(fmt_str),comm) ;
+            if (commcheck) {
+                if (ctx->local_port == DST_PORT){
+                    return SK_PASS; }
+            else {
+                return SK_DROP;
+            }
+            }
+    }}
+    return SK_PASS;
 }
